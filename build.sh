f252541302c64edf19a1bd607af64aa871406db6
Priyavardhanbezawada <priyavardhan.bezawada@gmail.com>
2025-08-27 23:59:16 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install system dependencies for Tesseract OCR
+apt-get update
+apt-get install -y tesseract-ocr
+
+# Install Python dependencies
+pip install -r requirements.txt
